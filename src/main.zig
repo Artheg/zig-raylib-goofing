b315347e77d27a6ee6a10d24c329137834a0a5fd
Artheg <artheg@pm.me>
2023-04-27 23:51:52 +0300

    Messing around with the types

--- src/main.zig
+++ src/main.zig
@@ -5,11 +5,12 @@ const raylib = @cImport({
 });
 pub fn main() !void {
     std.debug.print("Hello world!\n {}", .{raylib});
-
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
     raylib.InitWindow(640, 480, "__MF");
 
     while (!raylib.WindowShouldClose()) {
         raylib.BeginDrawing();
+        raylib.DrawText(alphabet, 320, 240, 11, raylib.BLACK);
         raylib.ClearBackground(raylib.WHITE);
         raylib.EndDrawing();
     }
