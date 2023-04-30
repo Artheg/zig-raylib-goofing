c3cfe889d67f5499fbb797c38085b8b44782517f
Artheg <artheg@pm.me>
2023-04-30 17:51:09 +0300

    wip

--- src/main.zig
+++ src/main.zig
@@ -5,14 +5,24 @@ const raylib = @cImport({
 });
 var x: i32 = 320;
 var y: i32 = 240;
+
+const Alphabet = struct {
+    letters: *const [26]u8,
+    pub fn getRandomLetter(self: Alphabet) u8 {
+        const index = std.rand.limitRangeBiased(usize, 1, self.letters.len);
+        std.debug.print("printing random letter\n", .{});
+        return self.letters[index];
+    }
+};
+
 pub fn main() !void {
-    std.debug.print("Hello world!\n {}", .{raylib});
-    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    // std.debug.print("Hello world!\n {}", .{raylib});
+    const alphabet = Alphabet{ .letters = "abcdefghijklmnopqrstuvwxyz" };
+    std.debug.print("letter: {}\n", .{alphabet.getRandomLetter()});
     raylib.InitWindow(640, 480, "__MF");
 
     while (!raylib.WindowShouldClose()) {
         // Look into DrawTextEx
-        // match?
         // convert
         raylib.BeginDrawing();
         x += switch (raylib.GetKeyPressed()) {
@@ -25,7 +35,7 @@ pub fn main() !void {
             raylib.KEY_DOWN => 1,
             else => 0,
         };
-        raylib.DrawText(alphabet, x, y, 11, raylib.BLACK);
+        // raylib.DrawText(alphabet.getRandomLetter(), x, y, 11, raylib.BLACK);
         raylib.ClearBackground(raylib.WHITE);
         raylib.EndDrawing();
     }
